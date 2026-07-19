d58d863a71f51b02a85356f4f976c19bf53086c4
Junaid Ilyas <h6539187@gmail.com>
2026-07-19 10:38:14 +0500

    Fix cross-site cookie policy for production

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,7 +13,7 @@ const isProd = process.env.NODE_ENV === "production";
 const options = {
   httpOnly: true,
   secure: isProd,
-  sameSite: "lax",
+  sameSite: isProd ? "none" : "lax",
   path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
