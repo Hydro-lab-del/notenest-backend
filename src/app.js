72b8c17088b168f6b337b7bdf5d8a0a6b81f3745
Junaid Ilyas <h6539187@gmail.com>
2026-07-19 10:46:51 +0500

    Fix express trust proxy for rate limiter

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
